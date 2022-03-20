25d645f2b96c54497abe763829f9af7da0fbc744
Andrey Kuznetsov <andrei.kuznetsov-andrey@yandex.ru>
2022-03-20 12:20:42 +0300

    Исправления после 2-го ревью. 5 проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,24 @@
 const template = document.querySelector('.template').content;
 const elements = document.querySelector('.elements');
-InitialCardsReversed = initialCards.reverse();
+initialCardsReversed = initialCards.reverse();
 
 function render() {
-  InitialCardsReversed.forEach(renderElement);
+  initialCardsReversed.forEach((card) => {
+    insertElement(renderElement(card));
+  });
 };
 
 function renderElement(text) {
   const newElement = template.cloneNode(true);
   const newFoto = newElement.querySelector('.element__foto');
   newFoto.src =  text.link;
+  newFoto.alt = text.name;
   newElement.querySelector('.element__caption').textContent = text.name;
   newElement.querySelector('.element__delete-button').addEventListener('click', hendleDelete);
   newFoto.addEventListener('click', () => openPopupImageViewFunc(text));
   newElement.querySelector('.element__like-button').addEventListener('click', function(event) {
     event.target.classList.toggle('element__like-button_active')});
-  insertElement (newElement);
+  return newElement;
 }
 
 render();
@@ -24,28 +27,26 @@ function insertElement (element) {
   elements.prepend(element);
 };
 
-
-
 //Profile popup
 
 const profileEditButton = document.querySelector ('.profile__edit-button');
 const profileName = document.querySelector ('.profile__name')
 const profileProfession = document.querySelector ('.profile__profession')
-const PopupProfile = document.querySelector ('.popup_profile');
+const popupProfile = document.querySelector ('.popup_profile');
 
-const formProfile = PopupProfile.querySelector ('.popup__container');
+const formProfile = popupProfile.querySelector ('.popup__container');
 const profilePopupClose = formProfile.querySelector ('.popup__close');
 const nameInput = formProfile.querySelector ('.popup__input_type_name');
 const jobInput = formProfile.querySelector ('.popup__input_type_profession');
 
 function openPopupProfileFunc () {
-  openPopupFunc(PopupProfile);
+  openPopupFunc(popupProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileProfession.textContent;
 }
 
 function closePopupProfileFunc () {
-  closePopupFunc (PopupProfile);
+  closePopupFunc (popupProfile);
 }
 
 function openPopupFunc (popup) {
@@ -60,7 +61,7 @@ function submitHandlerFormProfile (evt) {
   evt.preventDefault ();
   profileName.textContent = nameInput.value;
   profileProfession.textContent = jobInput.value;
-  closePopupFunc (PopupProfile);
+  closePopupFunc (popupProfile);
 };
 
 profileEditButton.addEventListener ('click', openPopupProfileFunc);
@@ -91,7 +92,7 @@ function submitHandlerFormNewElement(evt) {
       name: captionInput.value,
       link: urlInput.value
     };
-    renderElement(newCard);
+    insertElement(renderElement(newCard));
     closePopupFunc (popupNewElement);
   }
 }
@@ -114,19 +115,20 @@ function hendleDelete(event) {
 
 //popup_Image
 
-const PopupImageView = document.querySelector('.popup_Image');
-const popupImageViewClose = PopupImageView.querySelector('.popup__close');
-const viewUrl = PopupImageView.querySelector('.popup__foto');
-const viewCaption = PopupImageView.querySelector('.popup__caption');
+const popupImageView = document.querySelector('.popup_Image');
+const popupImageViewClose = popupImageView.querySelector('.popup__close');
+const viewUrl = popupImageView.querySelector('.popup__foto');
+const viewCaption = popupImageView.querySelector('.popup__caption');
 
 popupImageViewClose.addEventListener('click', сlosePopupImageViewFunc);
 
 function сlosePopupImageViewFunc() {
-  closePopupFunc(PopupImageView);
+  closePopupFunc(popupImageView);
 };
 
 function openPopupImageViewFunc(text) {
   viewUrl.src = text.link;
+  viewUrl.alt = text.name;
   viewCaption.textContent = text.name;
-  openPopupFunc(PopupImageView);
+  openPopupFunc(popupImageView);
 };
